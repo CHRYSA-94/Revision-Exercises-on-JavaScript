d504e80cd23052e2482916c400ebcedd7b427efe
Chrysa Lazaridou <chrysanthi.lazaridou@gmail.com>
2021-01-15 09:18:59 +0200

    all exercises are completed

--- index4.js
+++ index4.js
@@ -19,10 +19,10 @@
        const val = [];
        function twoDarray(evenNum) {
         
-         for(let i = 0; i < evenNum ; i++) {
+         for (let i = 0; i < evenNum ; i++) {
              val.push(i+1);
          }
-         for(let i = 0; i < evenNum  ; i++) {
+         for (let i = 0; i < evenNum  ; i++) {
             arr[i] = val ;
          }
        }
@@ -40,14 +40,18 @@
 // Exercise 5: Write a console statement that prints the properties of an
 // element p.
 
-        console.dir(document.getElementsByTagName('p')[0])
+        console.dir(document.getElementsByTagName('p'))
 // Exercise 6: Write a try catch statement that prints out an error. In
 // both cases it will also print out finally
-        // try {
-        //     x = 5;
-        // } catch(e) {
-        //     console.log(e)
-        // }
+
+        try {
+           conle.lg("hello")
+        } catch(error) {
+            console.log(error.name);
+            console.log(error.message);
+        } finally {
+            console.log("try catch statement is over")
+        }
 
 
 // Exercise 7: Write a switch statement that check and prints out which day
@@ -79,7 +83,40 @@
        }
        
 // Exercise 8: Write 5 different ways to check if an object is empty
+       const obj = {};
+        
+        if ( Object.keys(obj).length === 0) {
+            console.log("empty object")
+        }
+
+
+        if ( Object.entries(obj).length === 0) {
+            console.log("empty object")
+        }
+
+        if ( Object.values(obj).length === 0) {
+            console.log("empty object")
+        }
+        
+        (function checkIfNoProp() {
+            let count = 0;
+        for ( prop in obj) {
+            count++
+        }
+        if (count === 0){
+            console.log("empty object")
+        }
+        })();
        
 
 // Exercise 9: Write 4 different ways to find a value in an Array, write
 // your own smart examples
+        const colors = ["red", "black", "blue"];
+
+        console.log(colors.includes("blue"));
+        console.log(colors.indexOf("blue") !== -1);
+        console.log(colors.lastIndexOf("blue") !== -1);
+        console.log(colors.find( color => color === "blue") !== "undefined");
+       
+       
+       
